9d019cc6858147ed2345db9a6e4fa44c40af6a36
Pipe-25a <2felipeperez17@gmail.com>
2026-05-17 00:27:30 -0400

    Add files via upload

---
+++ Biblioteca/multa-service/multa-service/src/test/java/com/biblioteca/multa_service/MultaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioteca.multa_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
