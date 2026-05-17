19147ecbd04ba092d7ac0ef13a7d056af2ab58ac
Pipe-25a <2felipeperez17@gmail.com>
2026-05-17 19:45:45 -0400

    Delete Biblioteca/autenticacion-service/autenticacion-service directory

--- Biblioteca/autenticacion-service/autenticacion-service/src/main/java/com/biblioteca/autenticacion_service/repository/AutenticacionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.biblioteca.autenticacion_service.repository;
-
-import com.biblioteca.autenticacion_service.model.Autenticacion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AutenticacionRepository extends JpaRepository<Autenticacion, Long> {
-}
